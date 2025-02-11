6d483d428ea3cdd289db2487708500bc7db0b882
Beshimov03 <meylisbesimow44@gmail.com>
2025-02-12 02:30:59 +0300

    changed

--- beshimow.meylis/P6/main.cpp
+++ beshimow.meylis/P6/main.cpp
@@ -19,7 +19,6 @@ int main(int argc, char* argv[]) {
         std::cerr << "Ошибка: Необходимо ввести арифметическое выражение." << std::endl;
         return 1;
     }
-
     std::string expression;
     for (int i = 1; i < argc; ++i) {
         expression += argv[i];
